4a9873bb55e96b5723896904dc74f6921cff6284
Callylove <nwekechidimma88@gmail.com>
2025-06-13 16:38:15 +0100

    updated changes

--- src/services/getProductById.js
+++ src/services/getProductById.js
@@ -2,7 +2,7 @@ import { apiClient } from "./axios";
 
 export const getProductById = async (active_outlet,productID) => {
   try {
-    const data = await apiClient().get(`/api/v1/${active_outlet}/sephcocco_${active_outlet}_products/${productID} `);
+    const data = await apiClient().get(`/api/v1/${active_outlet}/sephcocco_${active_outlet}_products/${productID}?id=${productID}`);
     return data.data;
   } catch (err) {
     console.error(err);
