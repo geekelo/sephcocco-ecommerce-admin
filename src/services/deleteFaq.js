960f1989f937278977218734764af1567d553df3
Callylove <nwekechidimma88@gmail.com>
2025-07-02 16:18:15 +0100

    updated changes

--- src/services/deleteFaq.js
+++ src/services/deleteFaq.js
@@ -3,7 +3,7 @@ import { apiClient } from "./axios";
 
 export const deleteFaq = async (active_outlet,faqId) => {
   try {
-    const data = await apiClient().delete(`api/v1/${active_outlet}/sephcocco_${active_outlet}_faqs/id=${faqId}`);
+    const data = await apiClient().delete(`api/v1/${active_outlet}/sephcocco_${active_outlet}_faqs/${faqId}`);
     return data.data;
   } catch (err) {
     console.error(err);
