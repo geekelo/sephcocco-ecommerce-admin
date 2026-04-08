14cdd7552381932d0ec31b00236ba2ed1839f8ab
Samkeed <nwekechidimma88@gmail.com>
2026-04-08 21:36:43 +0100

    filters nested on completed orderS

--- src/services/getCompletedWaiterOrders.js
+++ src/services/getCompletedWaiterOrders.js
@@ -1,19 +1,21 @@
-import { apiClient } from "./axios";
-
 export const getCompletedWaiterOrders = async (active_outlet, page, per_page, filters = {}) => {
   try {
-    const params = { page, per_page };
-    if (filters.start_date) params.start_date = filters.start_date;
-    if (filters.end_date)   params.end_date   = filters.end_date;
+    const params = {
+      page,
+      per_page,
+      filters: {}
+    };
+
+    if (filters.start_date) params.filters.start_date = filters.start_date;
+    if (filters.end_date) params.filters.end_date = filters.end_date;
+
     const data = await apiClient().get(
       `api/v1/${active_outlet}/sephcocco_${active_outlet}_orders/completed`,
       { params }
     );
-    // console.log({DATA: data.data});
-    
-    return data.data;
-  } catch (err) {
-    console.error(err);
-    throw err;
+
+    return data;
+  } catch (error) {
+    throw error;
   }
 };
