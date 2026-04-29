7d07c14e8ce9fcd688e8dc61b9c96263d0727f9e
Eloghene <eloghene@bananacrystal.com>
2026-04-29 14:17:13 +0100

    fix: update API endpoint for fetching waiters

--- src/services/getWaiters.js
+++ src/services/getWaiters.js
@@ -2,7 +2,7 @@ import { apiClient } from "./axios";
 
 export const getWaiters = async () => {
   try {
-    const response = await apiClient().get(`/api/v1/get_waiters`);
+    const response = await apiClient().get(`/api/v1/sephcocco_users/get_waiters``);
     return response.data;
   } catch (err) {
     console.error(err);
